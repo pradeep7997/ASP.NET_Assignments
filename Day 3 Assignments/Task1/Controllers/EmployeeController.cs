ff16a131c80c85637840a56899bf6a5dc7c7f104
pradeep7997 <84910809+pradeep7997@users.noreply.github.com>
2022-07-27 14:37:15 +0530

    Add files via upload

---
+++ Day 3 Assignments/Task1/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Repository;
+using WebApplication3.Models;
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IEmployeeRepo _repo;
+        public EmployeeController(IEmployeeRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> Elist=_repo.GetAllDetails();
+            
+            return View(Elist);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            _repo.AddEmp(obj);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee obj = _repo.GetDetailsByID(id);
+            return View(obj);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Employee obj = _repo.GetDetailsByID(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee obj)
+        {
+            _repo.UpdateEmp(obj);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Employee obj = _repo.GetDetailsByID(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            _repo.DeleteEmp(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Day 3 Assignments/Task1/Repository/IEmployeeRepo.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication3.Models;
+namespace WebApplication3.Repository
+{
+    public interface IEmployeeRepo
+    {
+        public List<Models.Employee> GetAllDetails();
+        public Models.Employee GetDetailsByID(int id);
+        public void AddEmp(Models.Employee obj);
+        public void EditEmp(Models.Employee e1);
+        public void DeleteEmp(int id);
+        public void UpdateEmp(Models.Employee obj);
+
+    }
+}
